5dc2ee94dc4ddb13c4ff3487f6f902cd42d59649
Kate Hall and Rob King <samiamspop@gmail.com>
2016-01-26 14:03:40 -0800

    added scripts for multiple address and html

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,43 @@ function Contact(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
   this.addresses = [];
-}
+};
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
-}
-//interface
+};
+
+function Address(street, city, state, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zipCode = zipCode;
+};
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state + " " + this.zipCode;
+};
+
+// interface
 $(document).ready(function() {
+  $("#addAddress").click(function() {
+    $("#newAddresses").append('<div class="newAddress">' +
+                                 '<div class="form-group">' +
+                                   '<label for="newStreet">Street</label>' +                                    '<input id="newStreet" type="text" class="form-control">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="newCity">City</label>' +
+                                   '<input id="newCity" type="text" class="form-control">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="newState">State</label>' +
+                                   '<input id="newState" type="text" class="form-control">' +
+                                   '</div>' +
+                                   '<div class="form-group">' +
+                                     '<label for="newZip">State</label>' +
+                                     '<input id="newZip" type="text" class="form-control">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
   $("form#newContact").submit(function(event) {
     event.preventDefault();
 
@@ -16,16 +47,33 @@ $(document).ready(function() {
     var inputtedLastName = $("input#newLastname").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
+    $(".newAddress").each(function() {
+      var inputtedStreet = $(this).find("input#newStreet").val();
+      var inputtedCity = $(this).find("input#newCity").val();
+      var inputtedState = $(this).find("input#newState").val();
+      var inputtedZip = $(this).find("input#newZip").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip);
+      newContact.addresses.push(newAddress);
+    });
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+      $(".contact").last().click(function() {
+        $("#showContact").show();
+        $("#showContact h2").text(newContact.fullName());
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+        newContact.addresses.forEach(function(address) {
+           $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        });
+     });
 
     $("input#newFirstname").val("");
     $("input#newLastname").val("");
-
-    $(".contact").last().click(function() {
-      $("#showContact").show();
-      $("#showContact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-    });
+    $("input#newStreet").val("");
+    $("input#newCity").val("");
+    $("input#newState").val("");
+    $("input#newZip").val("");
+    $("addresses").empty();
   });
 });
